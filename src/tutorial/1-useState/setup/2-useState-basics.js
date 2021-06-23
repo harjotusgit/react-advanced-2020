e34bb80668cdfffb18be44f2a493bb7be2bbd5ee
harjotusgit <v4hv1stus@gmail.com>
2021-06-23 15:45:40 +0300

    Changed to ternary
    Changed if else to ternary

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -3,12 +3,11 @@ import React, { useState } from 'react';
 const UseStateBasics = () => {
   const [title, setTitle] = useState('random title');
   const handleClick = () => {
-    if (title === 'random title') {
-      setTitle('another title');
-    } else {
-      setTitle('random title');
-    }
+    title === 'random title'
+      ? setTitle('another title')
+      : setTitle('random title');
   };
+
   return (
     <>
       <h2>{title}</h2>
